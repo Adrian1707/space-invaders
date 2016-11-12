c615e38826023c292183ebdb853b8cfdbddeb202
Adrian Booth <adrian.booth@yourgolftravel.com>
2016-11-12 12:02:11 +0000

    set up keypress function to move spaceship on arrow key press. Currently only does it once

--- src/SpaceShip.js
+++ src/SpaceShip.js
@@ -8,6 +8,8 @@ class SpaceShip extends Component {
     this.state = {"bulletClass": "stable-bullet"}
     this.fire = this.fire.bind(this)
     this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.moveLeft = this.moveLeft.bind(this)
+    this.moveRight = this.moveRight.bind(this)
   }
 
   componentWillMount() {
@@ -24,15 +26,29 @@ class SpaceShip extends Component {
     }, 350)
   }
 
+  moveLeft(){
+    this.refs.ship1.style.marginLeft += "-5%"
+  }
+
+  moveRight(){
+    this.refs.ship1.style.marginLeft += "+5%"
+  }
+
   handleKeyDown(e){
-    if(e.keyCode == 32){
-      this.fire()
+    switch(e.keyCode){
+      case 32:
+        this.fire()
+        break;
+      case 39:
+        this.moveRight()
+      case 37:
+        this.moveLeft()
     }
   }
 
   render(){
     return (
-      <div onClick={this.fire} onKeyDown={this.handleKeyPress} className="ship">
+      <div onClick={this.fire} onKeyDown={this.handleKeyPress} className="ship" id="ship" ref="ship1">
         <Bullet bulletClass={this.state.bulletClass} />
       </div>
     )
